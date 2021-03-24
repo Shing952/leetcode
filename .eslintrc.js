7cc4d8d5edd318ab2904afacee5530b048e4d7a6
WuJiacheng <shing952@qq.com>
2021-03-24 21:49:35 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: module,
+  },
+};
